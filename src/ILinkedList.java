043f25a54fb151b229d05aebca2c3cc8f70ace10
Rick Wohlleb <rwohlleb1@gmail.com>
2018-01-18 08:27:05 -0500

    Adding finished work

--- src/ILinkedList.java
+++ src/ILinkedList.java
@@ -1,4 +1,70 @@
-package PACKAGE_NAME;
+public class ILinkedList<T> implements ILinkedListInterface<T>
+{
+    private ILinkedList<T> head;
+    private T nodeData;
 
-public class ILinkedList {
+    public ILinkedList(){}
+
+    public ILinkedList (T data) {
+        head = null;
+        this.nodeData = data;
+    }
+
+    @Override
+    public ILinkedList<T> next() {
+        return this.head;
+    }
+
+    @Override
+    public ILinkedList<T> last() {
+        ILinkedList<T> temp = this;
+
+        while(temp.head != null){
+            temp = temp.head;
+        }
+        return temp;
+    }
+
+    @Override
+    public ILinkedList<T> after(int n) {
+        ILinkedList temp = this;
+
+        int count = 0;
+        while(count < n){
+            temp = temp.head;
+            count++;
+        }
+        return temp;
+    }
+
+    public ILinkedList<T> detach() {
+        ILinkedList temp2 = this.head;
+        this.head = null;
+        return temp2;
+    }
+
+    public T get() {
+        return this.nodeData;
+    }
+
+    public void set(T data) {
+        this.nodeData = data;
+    }
+
+    @Override
+    public void setNext(ILinkedList<T> next) {
+        this.head = next;
+    }
+
+    @Override
+    public void append(ILinkedList<T> next) {
+        ILinkedList temp = this;
+        temp = temp.last();
+        temp.head = next;
+    }
+
+    @Override
+    public void insert(ILinkedList<T> newFirst) {
+        newFirst.head = this;
+    }
 }
--- src/ILinkedListTest.java
+++ src/ILinkedListTest.java
@@ -1,5 +1,103 @@
+import org.junit.jupiter.api.Assertions;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ILinkedListTest {
 
+    @org.junit.jupiter.api.Test
+    void next()
+    {
+        ILinkedList<Integer> temp = new ILinkedList(10);
+        temp.append(new ILinkedList<>(20));
+        ILinkedList expected = null;
+        ILinkedList result = temp.next().next();
+        Assertions.assertEquals(expected, result);
+    }
+
+    @org.junit.jupiter.api.Test
+    void last()
+    {
+        ILinkedList<Integer> temp = new ILinkedList<>(10);
+        temp.append(new ILinkedList<>(20));
+        int expected = 20;
+        ILinkedList result = temp.last();
+        Assertions.assertEquals(expected,result.get());
+    }
+
+    @org.junit.jupiter.api.Test
+    void after()
+    {
+        ILinkedList temp = new ILinkedList(10);
+        ILinkedList next = new ILinkedList(20);
+        temp.setNext(next);
+        next = new ILinkedList(30);
+        temp.append(next);
+        int expected = 30;
+        next = temp.after(2);
+        Assertions.assertEquals(expected, next.get());
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void detach()
+    {
+        ILinkedList temp = new ILinkedList(10);
+        ILinkedList next = new ILinkedList(20);
+        temp.setNext(next);
+        next = new ILinkedList(30);
+        temp.setNext(next);
+        int expected = 30;
+        Assertions.assertEquals(expected, next.get());
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void get()
+    {
+        ILinkedList temp = new ILinkedList(10);
+        int expected = 10;
+        Assertions.assertEquals(expected, temp.get());
+    }
+
+    @org.junit.jupiter.api.Test
+    void set()
+    {
+        ILinkedList temp = new ILinkedList();
+        temp.set(10);
+        int expected = 10;
+        Assertions.assertEquals(expected, temp.get());
+    }
+
+    @org.junit.jupiter.api.Test
+    void setNext()
+    {
+        ILinkedList temp = new ILinkedList(10);
+        ILinkedList next = new ILinkedList(20);
+        temp.setNext(next);
+        int expected = 20;
+        Assertions.assertEquals(expected, temp.next().get());
+    }
+
+    @org.junit.jupiter.api.Test
+    void append()
+    {
+        ILinkedList temp = new ILinkedList(10);
+        ILinkedList next = new ILinkedList(20);
+        temp.append(next);
+        next = new ILinkedList(30);
+        temp.append(next);
+        int expected = 30;
+        ILinkedList result = temp.last();
+        Assertions.assertEquals(expected,result.get());
+    }
+
+    @org.junit.jupiter.api.Test
+    void insert()
+    {
+        ILinkedList temp = new ILinkedList(10);
+        ILinkedList newNode = new ILinkedList(20);
+        temp.insert(newNode);
+        int expected = 10;
+        Assertions.assertEquals(expected, temp.get());
+    }
 }
